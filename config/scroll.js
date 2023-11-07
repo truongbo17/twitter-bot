659d76fbf61b9f73b79551f1f19524901453919a
Nguyen Quang Truong <truongnq017@gmail.com>
2023-11-07 23:10:47 +0700

    add rule

---
+++ config/scroll.js
@@ -0,0 +1,35 @@
+// Cuộn rất nhanh và rất mượt
+const scroll_level_1 = {
+  'timeout': 1,
+  'pixel': 1
+};
+
+// Cuộn rất nhanh và mượt
+const scroll_level_2 = {
+  'timeout': 1,
+  'pixel': 10
+};
+
+// Cuộn bình thường
+const scroll_level_3 = {
+  'timeout': 10,
+  'pixel': 1
+};
+
+// Cuộn bình thường và khá giật
+export const scroll_level_4 = {
+  'timeout': 100,
+  'pixel': 5
+};
+
+// Cuộn khá chậm và giật
+export const scroll_level_5 = {
+  'timeout': 200,
+  'pixel': 20
+};
+
+// Cuộn cực chậm
+export const scroll_level_6 = {
+  'timeout': 200,
+  'pixel': 1
+};
