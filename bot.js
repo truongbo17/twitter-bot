dacdc1f38c74b13c681e7df594013de8d738eaf7
Nguyen Quang Truong <truongnq017@gmail.com>
2023-11-07 22:07:18 +0700

    add rule

--- bot.js
+++ bot.js
@@ -1,19 +1,30 @@
 const startBot = document.getElementById('startBot');
 const stopBot = document.getElementById('stopBot');
-let timeout;
-const pixels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let runScroll;
+
+const configScroll = [
+
+];
 
 startBot.onclick = startBotPlay;
 stopBot.onclick = stopBotPlay;
 
-function startBotPlay() {
-  clearTimeout(timeout);
-
-  timeout = setInterval(scroll, 100);
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function scroll() {
-  executeScript('window.scrollBy(0, ' + 5 + ')');
+function startBotPlay() {
+  const scrollDirection = getRandomInt(0, 1) === 0 ? 1 : -1; // Ngẫu nhiên chọn hướng cuộn lên hoặc xuống
+  const scrollAmount = getRandomInt(1, 5); // Ngẫu nhiên chọn số pixel cuộn (1-5, tùy chỉnh theo nhu cầu)
+
+  scroll(scrollDirection * scrollAmount)
+
+  const waitTime = getRandomInt(1000, 6000); // Ngẫu nhiên thời gian chờ (1 giây - 6 giây, tùy chỉnh theo nhu cầu)
+  setTimeout(startBotPlay, waitTime);
+}
+
+function scroll(scrollTo) {
+  executeScript('window.scrollBy(0, ' + scrollTo + ')');
 }
 
 function executeScript(script) {
