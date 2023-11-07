5df8baf4ec183a5cecfc442aac496eac5fa35f85
Nguyen Quang Truong <truongnq017@gmail.com>
2023-11-07 23:10:32 +0700

    add rule

--- bot.js
+++ bot.js
@@ -2,30 +2,76 @@ const startBot = document.getElementById('startBot');
 const stopBot = document.getElementById('stopBot');
 let runScroll;
 
-const configScroll = [
+// Cuộn rất nhanh và rất mượt
+const scroll_level_1 = {
+  'timeout': 1,
+  'pixel': 1
+};
 
-];
+// Cuộn rất nhanh và mượt
+const scroll_level_2 = {
+  'timeout': 1,
+  'pixel': 10
+};
+
+// Cuộn bình thường
+const scroll_level_3 = {
+  'timeout': 10,
+  'pixel': 1
+};
+
+// Cuộn bình thường và khá giật
+const scroll_level_4= {
+  'timeout': 100,
+  'pixel': 5
+};
+
+// Cuộn khá chậm và giật
+const scroll_level_5= {
+  'timeout': 200,
+  'pixel': 20
+};
+
+// Cuộn cực chậm
+const scroll_level_6= {
+  'timeout': 200,
+  'pixel': 1
+};
+
 
 startBot.onclick = startBotPlay;
 stopBot.onclick = stopBotPlay;
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function startBotPlay() {
-  const scrollDirection = getRandomInt(0, 1) === 0 ? 1 : -1; // Ngẫu nhiên chọn hướng cuộn lên hoặc xuống
-  const scrollAmount = getRandomInt(1, 5); // Ngẫu nhiên chọn số pixel cuộn (1-5, tùy chỉnh theo nhu cầu)
+  clearTimeout(runScroll);
 
-  scroll(scrollDirection * scrollAmount)
-
-  const waitTime = getRandomInt(1000, 6000); // Ngẫu nhiên thời gian chờ (1 giây - 6 giây, tùy chỉnh theo nhu cầu)
-  setTimeout(startBotPlay, waitTime);
+  runScroll = setInterval(scroll, scroll_level_3.timeout);
 }
 
-function scroll(scrollTo) {
-  executeScript('window.scrollBy(0, ' + scrollTo + ')');
+function scroll() {
+  executeScript('window.scrollBy(0, ' + scroll_level_3.pixel + ')');
 }
+
+window.addEventListener('scroll', function() {
+  let elementsInViewport = getElementsInViewport();
+  console.log('Elements in viewport:', elementsInViewport);
+});
+function getElementsInViewport() {
+  const viewportHeight = window.innerHeight;
+  const elements = document.querySelectorAll('.your-element-class'); // Thay '.your-element-class' bằng lựa chọn phù hợp
+
+  const elementsInViewport = [];
+
+  elements.forEach(function(element) {
+    const elementRect = element.getBoundingClientRect();
+    if (elementRect.top < viewportHeight && elementRect.bottom >= 0) {
+      elementsInViewport.push(element);
+    }
+  });
+
+  return elementsInViewport;
+}
+
 
 function executeScript(script) {
   chrome.tabs.executeScript({
