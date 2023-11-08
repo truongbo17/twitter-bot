04936d8229ba38cbfbc50165772a8e1ad050c599
truongnq72 <truongnq72@ghtk.co>
2023-11-08 17:45:51 +0700

    build: update

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const startBot = document.getElementById('startBot');
 const stopBot = document.getElementById('stopBot');
-let runScroll;
+let runScroll, currentTab;
 
 // Cuộn rất nhanh và rất mượt
 const scroll_level_1 = {
@@ -38,6 +38,14 @@ const scroll_level_6= {
   'pixel': 1
 };
 
+document.addEventListener("DOMContentLoaded", function(event) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    const tab = tabs[0];
+    if (tab) {
+      currentTab = tab;
+    }
+  });
+});
 
 startBot.onclick = startBotPlay;
 stopBot.onclick = stopBotPlay;
@@ -45,31 +53,23 @@ stopBot.onclick = stopBotPlay;
 function startBotPlay() {
   clearTimeout(runScroll);
 
+  chrome.scripting.executeScript({
+    target: { tabId: currentTab.id },
+    func: () => {
+      const elements = document.querySelectorAll(".title-news");
+      const elementsInViewport = Array.from(elements).filter(element => {
+        const rect = element.getBoundingClientRect();
+        return (rect.top >= 0 && rect.left >= 0 && rect.right <= window.innerWidth && rect.bottom <= window.innerHeight);
+      });
+      console.log(elementsInViewport)
+    },
+  });
+
   runScroll = setInterval(scroll, scroll_level_3.timeout);
 }
 
 function scroll() {
   executeScript('window.scrollBy(0, ' + scroll_level_3.pixel + ')');
-}
-
-window.addEventListener('scroll', function() {
-  let elementsInViewport = getElementsInViewport();
-  console.log('Elements in viewport:', elementsInViewport);
-});
-function getElementsInViewport() {
-  const viewportHeight = window.innerHeight;
-  const elements = document.querySelectorAll('.your-element-class'); // Thay '.your-element-class' bằng lựa chọn phù hợp
-
-  const elementsInViewport = [];
-
-  elements.forEach(function(element) {
-    const elementRect = element.getBoundingClientRect();
-    if (elementRect.top < viewportHeight && elementRect.bottom >= 0) {
-      elementsInViewport.push(element);
-    }
-  });
-
-  return elementsInViewport;
 }
 
 
